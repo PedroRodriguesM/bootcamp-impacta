996ba429b9a08421b80a2a4f26f93f539ce801c8
Pedro Rodrigues M. da Silva <pedrorodriguesmoraesdasilva@outlook.com>
2021-09-01 16:51:54 -0300

    Estrutura de Seleção

--- Aula01/HelloWorld/HelloWorld/Program.cs
+++ Aula01/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,43 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int idade = 32;
+            bool isAdulto = true;
+            decimal valor = 10.55m;
+            double valor2 = 10.55;
+            char sigla = 'C';
+            string estado = "Sao Paulo";
+
+            var teste = "Teste";
+
+            if (idade >= 18)
+            {
+                teste = "Maior de Idade";
+            }
+            else if(estado == "Sao Paulo")
+            {
+                teste = "Menor de idade";
+            }
+
+            Console.WriteLine(teste);
+
+            switch (idade)
+            {
+                case 2:
+                    teste = "Nenem";
+                    break;
+                case 18:
+                    teste = "Crianca";
+                    break;
+                case 65:
+                    teste = "Adulto";
+                    break;
+                default: 
+                    teste = "Melhor idade";
+                    break;
+
+            }
+
         }
     }
 }
