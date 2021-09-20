eb5c9998bb31ba3ee72d0ab5f4b5c54537a5a998
Pedro Rodrigues M. da Silva <pedrorodriguesmoraesdasilva@outlook.com>
2021-09-20 15:56:45 -0300

    TesteEf

---
+++ Aula09/TesteEf/Migrations/20210920184020_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TesteEf.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_USER",
+                columns: table => new
+                {
+                    ID_USER = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NM_USER = table.Column<string>(type: "VARCHAR(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_USER", x => x.ID_USER);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_BLOG",
+                columns: table => new
+                {
+                    ID_BLOG = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_USER_BLOG = table.Column<int>(type: "int", nullable: false),
+                    DSC_BLOG = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    DT_CREATED = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_BLOG", x => x.ID_BLOG);
+                    table.ForeignKey(
+                        name: "FK_TB_BLOG_TB_USER_ID_USER_BLOG",
+                        column: x => x.ID_USER_BLOG,
+                        principalTable: "TB_USER",
+                        principalColumn: "ID_USER",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TB_BLOG_ID_USER_BLOG",
+                table: "TB_BLOG",
+                column: "ID_USER_BLOG");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_BLOG");
+
+            migrationBuilder.DropTable(
+                name: "TB_USER");
+        }
+    }
+}
---
+++ Aula09/TesteEf/TesteEf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteEf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
