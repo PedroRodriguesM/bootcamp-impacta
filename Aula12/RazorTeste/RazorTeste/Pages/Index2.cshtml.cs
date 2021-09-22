339962052707b2545e8b3f22881599f93179b0e1
Pedro Rodrigues M. da Silva <pedrorodriguesmoraesdasilva@outlook.com>
2021-09-22 18:07:10 -0300

    Razor e MVC

---
+++ Aula12/RazorTeste/RazorTeste/Pages/Index2.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorTeste.Pages
+{
+    public class Index2Model : PageModel
+    {
+        public string Message { get; private set; }
+
+        public void OnGet()
+        {
+            Message = "Horario do Servidor é: " + DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToShortTimeString();
+        }
+    }
+}
