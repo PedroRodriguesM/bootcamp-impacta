4b9a493eeed328bbf48f5d4041ccec49c1e30077
Pedro Rodrigues M. da Silva <pedrorodriguesmoraesdasilva@outlook.com>
2021-10-01 13:54:10 -0300

    Revert "Inicio API"
    This reverts commit f62e5a7f2fcf2d558220bfca45eb34a90969f2d1.

---
+++ Aula15/ScaffoldMercado/Data/ScaffoldMercadoContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ScaffoldMercado.Models;
+
+namespace ScaffoldMercado.Data
+{
+    public class ScaffoldMercadoContext : DbContext
+    {
+        public ScaffoldMercadoContext (DbContextOptions<ScaffoldMercadoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ScaffoldMercado.Models.Produto> Produto { get; set; }
+
+        public DbSet<ScaffoldMercado.Models.Usuario> Usuario { get; set; }
+        public DbSet<ScaffoldMercado.Models.VendaItem> VendaItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        public DbSet<ScaffoldMercado.Models.Venda> Venda { get; set; }
+    }
+}
